817a567533a5b807997bff3b83d9a75be84b473c
KKFL tba <kkfltba2@gmail.com>
2023-04-12 11:38:46 +0300

    .

--- TasarimRepo/MainWindow.xaml.cs
+++ TasarimRepo/MainWindow.xaml.cs
@@ -1,7 +1,9 @@
 ﻿using System.Collections.ObjectModel;
 using System.Windows;
+using System.Windows.Controls;
 using System.Windows.Input;
 using System.Windows.Media;
+using System.Xml.Linq;
 
 namespace TasarimRepo
 {
@@ -11,36 +13,64 @@ namespace TasarimRepo
         public MainWindow()
         {
             InitializeComponent();
+            this.WindowState = WindowState.Maximized;
+        }
+        #region
+        //private void Border_MouseDown(object sender, MouseButtonEventArgs e)
+        //{
+        //    if (e.ChangedButton == MouseButton.Left)
+        //    {
+        //        this.DragMove();
+        //    }
+        //}
+        //private bool IsMaximized = false;
+        //private void Border_MouseLeftButtonDown(object sender, MouseButtonEventArgs e)
+        //{
+        //    if (e.ClickCount == 2)
+        //    {
+        //        if (IsMaximized)
+        //        {
+        //            this.WindowState = WindowState.Normal;
+        //            this.Width = 1080;
+        //            this.Height = 720;
+        //
+        //            IsMaximized = false;
+        //        }
+        //        else
+        //        {
+        //            this.WindowState = WindowState.Maximized;
+        //
+        //
+        //            IsMaximized = true;
+        //        }
+        //    }
+        //}
+        #endregion
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            panel2.Visibility = Visibility.Visible;
+            panel1.Visibility = Visibility.Hidden;
         }
 
-        private void Border_MouseDown(object sender, MouseButtonEventArgs e)
+        private void Button_Click_2(object sender, RoutedEventArgs e)
         {
-            if (e.ChangedButton == MouseButton.Left)
-            {
-                this.DragMove();
-            }
+
         }
-        private bool IsMaximized = false;
-        private void Border_MouseLeftButtonDown(object sender, MouseButtonEventArgs e)
+
+        private void Button_Click_3(object sender, RoutedEventArgs e)
         {
-            if (e.ClickCount == 2)
-            {
-                if (IsMaximized)
-                {
-                    this.WindowState = WindowState.Normal;
-                    this.Width = 1080;
-                    this.Height = 720;
 
-                    IsMaximized = false;
-                }
-                else
-                {
-                    this.WindowState = WindowState.Maximized;
+        }
 
+        private void Button_Click_1(object sender, RoutedEventArgs e)
+        {
 
-                    IsMaximized = true;
-                }
-            }
+        }
+
+        private void Button_Click_4(object sender, RoutedEventArgs e)
+        {
+            panel1.Visibility = Visibility.Visible;
+            panel2.Visibility = Visibility.Hidden;
         }
     }
 }
